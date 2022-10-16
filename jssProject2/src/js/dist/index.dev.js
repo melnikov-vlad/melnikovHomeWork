ea4b0bda76720a37555631f19aabf7219cb0b250
melnikov-vlad <melnikovvladislavzzz@gmail.com>
2022-10-17 00:33:57 +0300

    17.10.22

--- jssProject2/src/js/dist/index.dev.js
+++ jssProject2/src/js/dist/index.dev.js
@@ -1,12 +1,18 @@
 "use strict";
 
-$(document).ready(function () {
-  $('.main_wrapper').slick({
-    setting_name: setting - value
+// $(document).ready(function(){
+//   $('.main_wrapper').slick({
+//   });
+// });
+// $('.single-item').slick({
+//   dots: true,
+//   arrows: false,
+//   focusOnSelect: true,
+// }
+// );
+$(function () {
+  $('.header_slider').slick({
+    dots: true,
+    arrows: false
   });
-});
-$('.single-item').slick({
-  dots: true,
-  arrows: false,
-  focusOnSelect: true
 });
--- jssProject2/src/js/index.js
+++ jssProject2/src/js/index.js
@@ -1,14 +1,23 @@
-$(document).ready(function(){
-  $('.main_wrapper').slick({
-    setting_name: setting-value
-  });
-});
+// $(document).ready(function(){
+//   $('.main_wrapper').slick({
+    
+//   });
+// });
 
-$('.single-item').slick({
-  dots: true,
-  arrows: false,
-  focusOnSelect: true,
+
+
+// $('.single-item').slick({
+//   dots: true,
+//   arrows: false,
+//   focusOnSelect: true,
 
   
-}
-);
+// }
+// );
+
+$(function () {
+  $('.header_slider').slick({
+      dots: true,
+      arrows: false,
+    })
+})
